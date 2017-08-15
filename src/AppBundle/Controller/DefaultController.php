54a48949cbd8b1e150284560b8985ba35ca5f61d
vlad242 <vlad242@i.ua>
2017-08-15 17:29:40 +0300

    Cut registerAction from UserController in DefaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\User_newType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DefaultController extends Controller
@@ -45,5 +48,31 @@ class DefaultController extends Controller
     {
         // this controller will not be executed,
         // as the route is handled by the Security system
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user= new User();
+        $form = $this->createForm(User_newType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $user->setRole("ROLE_USER");
+            $user->setLocked(false);
+            $user->setEnabled(true);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render(':User:new_user.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
